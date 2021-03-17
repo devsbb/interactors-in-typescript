fd5e8a8658af2bc35b3823d0a4b358f2015690d3
Moritz Rumpf <moritz.rumpf@gmail.com>
2021-03-17 14:46:14 +0100

    Define Interactors

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,13 @@ function organize(
     b()
     c()
 }
+
+const getShoppingCard: Interactor = () => {
+    console.log("loading shopping cart")
+}
+const calculateOffer: Interactor = () => {
+    console.log("calculating offer")
+}
+const sendOffer: Interactor = () => {
+    console.log("sending offer")
+}
