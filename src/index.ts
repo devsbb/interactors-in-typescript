7275b1ad84e90cfa16f5156fb209979bc7b30d0e
Moritz Rumpf <moritz.rumpf@gmail.com>
2021-03-17 14:49:36 +0100

    Make interactors failable
    and only run following interactors in case the previous one was successful

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,74 @@
 type Interactor<Before, After> = (contextBefore: Before) => After
 
+type Failable<T> =  { success: true, value: T } | { success: false }
+type FailableInteractor<Before, After> = (contextBefore: Before) => Failable<After>
+
+
 function organize<A, B, C, D>(
     startContext: A,
-    a: Interactor<A, B>,
-    b: Interactor<B, C>,
-    c: Interactor<C, D>
+    a: FailableInteractor<A, B>,
+    b: FailableInteractor<B, C>,
+    c: FailableInteractor<C, D>
 ) {
-    c(b(a(startContext)))
+    const resultA = a(startContext)
+    if (resultA.success) {
+        const resultB = b(resultA.value)
+
+        if (resultB.success) {
+            c(resultB.value)
+        }
+    }
 }
 
 
 type User = string
 type Cart = Array<{ price: number, name: string }>
 
-const getShoppingCard: Interactor<User, Cart> = user => {
+const getShoppingCard: FailableInteractor<User, Cart> = user => {
     console.log("loading shopping cart for user", user)
-    return [
-        { name: "IPhone", price: 1000 },
-        { name: "headphones", price: 100 }
-    ]
+    if (user === "Thief") {
+        return {
+            success: false,
+        }
+    }
+    return {
+        success: true,
+        value: [
+            { name: "IPhone", price: 1000 },
+            { name: "headphones", price: 100 }
+        ]
+    }
 }
 
 type Offer = {
     total: number
 }
 
-const calculateOffer: Interactor<Cart, Offer> = cart => {
+const calculateOffer: FailableInteractor<Cart, Offer> = cart => {
     console.log("calculating offer")
     return {
-        total: cart.reduce((total, item) => total + item.price, 0)
+        success: true,
+        value: {
+            total: cart.reduce((total, item) => total + item.price, 0)
+        }
     }
 }
-const sendOffer: Interactor<Offer, void> = offer => {
+
+const internetIsBad = false
+const sendOffer: FailableInteractor<Offer, void> = offer => {
     console.log("sending offer:", offer)
+    if (internetIsBad) {
+        console.log("failed to send offer")
+        return {
+            success: false
+        }
+    } else {
+        console.log("successfully sent offer!")
+        return {
+            success: true,
+            value: undefined
+        }
+    }
 }
 
 
