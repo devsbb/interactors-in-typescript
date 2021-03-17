921038fe094aca4d48e91e891bf8d475e71a1078
Moritz Rumpf <moritz.rumpf@gmail.com>
2021-03-17 14:47:25 +0100

    Add input/output to organizers

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,44 @@
-type Interactor = () => void
+type Interactor<Before, After> = (contextBefore: Before) => After
 
-function organize(
-    a: Interactor,
-    b: Interactor,
-    c: Interactor
+function organize<A, B, C, D>(
+    startContext: A,
+    a: Interactor<A, B>,
+    b: Interactor<B, C>,
+    c: Interactor<C, D>
 ) {
-    a()
-    b()
-    c()
+    c(b(a(startContext)))
 }
 
-const getShoppingCard: Interactor = () => {
-    console.log("loading shopping cart")
+
+type User = string
+type Cart = Array<{ price: number, name: string }>
+
+const getShoppingCard: Interactor<User, Cart> = user => {
+    console.log("loading shopping cart for user", user)
+    return [
+        { name: "IPhone", price: 1000 },
+        { name: "headphones", price: 100 }
+    ]
 }
-const calculateOffer: Interactor = () => {
+
+type Offer = {
+    total: number
+}
+
+const calculateOffer: Interactor<Cart, Offer> = cart => {
     console.log("calculating offer")
+    return {
+        total: cart.reduce((total, item) => total + item.price, 0)
+    }
 }
-const sendOffer: Interactor = () => {
-    console.log("sending offer")
+const sendOffer: Interactor<Offer, void> = offer => {
+    console.log("sending offer:", offer)
 }
+
+
 
 organize(
+    "Tina",
     getShoppingCard,
     calculateOffer,
     sendOffer
